d9f2ac246715db0a3d60252f01ad7cef33764616
程 <1303842899@qq.com>
2022-04-26 14:02:57 +0800

    插入jq

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-// console.log("$", $);
+console.log("dom start", document.querySelector(".deploy__price"));
 const ASSET_POINT_UPSELL = "https://www.test.com";
 const API_POINT_ASSET_POINT_UPSELL = "https://develop-lf-bundle-selling.lfszo.codefriend.top"; // 测试环境
 let theme = window.current_theme || window.localStorage.getItem("current_theme"); // 当前的主题
 if (!theme) {
   theme = document.querySelector("#appjs") ? document.querySelector("#appjs").getAttribute("currentTheme") : "vogue";
 }
+// 插入jq
+// let script = document.createElement("script");
+// script.type = "text/javascript";
+// script.src = "https://www.test.com/jq-3.6.0.min.js";
+// document.head.appendChild(script);
 window.addEventListener("load", () => {
   console.log("dom vogue ", document.querySelector(".deploy__price"));
   console.log("jq是否加载成功", $);
